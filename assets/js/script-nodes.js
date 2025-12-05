66687a2a310e423c97f90586beb0e95bfb85beee
Raul Gomez Hernandez <raulgomhernandez@gmail.com>
2025-12-05 06:16:02 +0100

    Update script-nodes.js

--- assets/js/script-nodes.js
+++ assets/js/script-nodes.js
@@ -349,7 +349,7 @@ document.getElementById('chatForm').addEventListener('submit',e=>{
   const b=selectB.value;
   
   if(!b) {
-    return appendMsg('Please select a colleague from the menu on the right.');
+    return appendMsg('Please select a person from the menu on the right.');
   }
     
   // *** AUTO-CLEAR PREVIOUS RESULT ***
@@ -363,5 +363,5 @@ document.getElementById('chatForm').addEventListener('submit',e=>{
 // Clear button handler
 document.getElementById('clear').addEventListener('click',()=>{
   chatLog.innerHTML='';
-  appendMsg('Select a colleague and press "Query Relationship" to see the bond with Charles Lyell.');
+  appendMsg('Select a person and press "Query Relationship" to see the bond with Charles Lyell.');
 });
